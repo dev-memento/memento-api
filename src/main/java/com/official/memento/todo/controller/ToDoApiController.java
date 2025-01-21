ef899d41bcd30b3476fb86dbd6c191545aa99e6d
unanchoi <unanc2@gmail.com>
2025-01-21 16:19:41 +0900

    fix: authorization user 주석 삭제 및 파라미터로 추가

--- src/main/java/com/official/memento/todo/controller/ToDoApiController.java
+++ src/main/java/com/official/memento/todo/controller/ToDoApiController.java
@@ -1,5 +1,6 @@
 package com.official.memento.todo.controller;
 
+import com.official.memento.global.annotation.Authorization;
 import com.official.memento.global.annotation.AuthorizationUser;
 import com.official.memento.global.dto.SuccessResponse;
 import com.official.memento.global.entity.enums.RepeatOption;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/todos")
-public class ToDoApiController implements ToDoApiDocs {
+public class ToDoApiController {
 
     private final ToDoCreateUseCase toDoCreateUseCase;
     private final ToDoDeleteUseCase toDoDeleteUseCase;
@@ -35,7 +36,6 @@ public class ToDoApiController implements ToDoApiDocs {
     }
 
     @PostMapping
-    @Override
     public ResponseEntity<SuccessResponse<?>> createToDo(
             @Authorization final AuthorizationUser authorizationUser,
             @RequestBody final ToDoCreateRequest request
@@ -59,16 +59,12 @@ public class ToDoApiController implements ToDoApiDocs {
     }
 
     @DeleteMapping("/{toDoId}")
-    @Override
     public ResponseEntity<SuccessResponse<?>> deleteToDo(
             @Authorization final AuthorizationUser authorizationUser,
             @PathVariable final long toDoId
     ) {
         // todo: 로그인 후 추후 삭제 예정
-        toDoDeleteUseCase.delete(ToDoDeleteCommand.of(
-                authorizationUser.memberId(),
-                toDoId)
-        );
+        toDoDeleteUseCase.delete(ToDoDeleteCommand.of(authorizationUser.memberId(), toDoId));
         return SuccessResponse.of(
                 HttpStatus.OK,
                 "단일 스케줄 삭제 성공"
@@ -76,7 +72,7 @@ public class ToDoApiController implements ToDoApiDocs {
     }
 
     @PatchMapping("/{toDoId}")
-    ResponseEntity<SuccessResponse<?>> updateToDo(
+    public ResponseEntity<SuccessResponse<?>> updateToDo(
             @Authorization final AuthorizationUser authorizationUser,
             @PathVariable final long toDoId,
             @RequestBody final ToDoUpdateRequest request
