1344a3f82e4d1f64484c3fb2e623b298bb50462e
paragon0107 <smg0725@naver.com>
2025-01-23 02:28:19 +0900

    feat: Tag 레포지토리 메서드 추가

--- src/main/java/com/official/memento/tag/domain/TagRepository.java
+++ src/main/java/com/official/memento/tag/domain/TagRepository.java
@@ -1,9 +1,15 @@
 package com.official.memento.tag.domain;
 
+import com.official.memento.tag.domain.enums.TagColor;
+
 import java.util.List;
 
 public interface TagRepository {
     Tag save(Tag tag);
+
     Tag findById(Long id);
+
     List<Tag> findAllByMemberId(Long memberId);
+
+    Tag findByMemberIdAndTagColor(Long memberId, TagColor tagColor);
 }
