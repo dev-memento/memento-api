65be67bddcb0a1131eac426e374d1f054dcd21bd
unanchoi <unanc2@gmail.com>
2025-01-24 07:00:18 +0900

    fix: alarm 처리 코드 추가

--- src/main/java/com/official/memento/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/official/memento/global/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.official.memento.global.handler;
 
 import com.official.memento.alarm.domain.port.AlarmOutputPort;
+import com.official.memento.alarm.service.command.AlarmExceptionCommand;
 import com.official.memento.alarm.service.command.AlarmSendUseCase;
 import com.official.memento.global.dto.ErrorResponse;
 import com.official.memento.global.exception.*;
@@ -23,37 +24,42 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> exception(Exception exception) {
         logger.error("Unhandled exception occurred ", exception);
-        alarmSendUseCase.sendExceptionAlarm(Alar);
+        alarmSendUseCase.sendException(new AlarmExceptionCommand(exception));
         return ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(MementoException.class)
     public ResponseEntity<ErrorResponse> mementoException(MementoException exception) {
         logger.error("MementoException", exception);
+        alarmSendUseCase.sendException(new AlarmExceptionCommand(exception));
         return ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(InvalidRequestBodyException.class)
     public ResponseEntity<ErrorResponse> invalidRequestBodyException(InvalidRequestBodyException exception) {
         logger.warn("Invalid request body: {}", exception.getMessage());
+        alarmSendUseCase.sendException(new AlarmExceptionCommand(exception));
         return ErrorResponse.of(HttpStatus.BAD_REQUEST, ErrorCode.INVALID_REQUEST_BODY);
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ErrorResponse> unauthorizedException(UnauthorizedException exception) {
         logger.error("Unauthorized access attempt", exception);
+        alarmSendUseCase.sendException(new AlarmExceptionCommand(exception));
         return ErrorResponse.of(HttpStatus.UNAUTHORIZED, ErrorCode.UNAUTHORIZED_USER);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponse> entityNotFoundException(EntityNotFoundException exception) {
         logger.error("Entity not found:", exception);
+        alarmSendUseCase.sendException(new AlarmExceptionCommand(exception));
         return ErrorResponse.of(HttpStatus.NOT_FOUND, ErrorCode.NOT_FOUND_ENTITY);
     }
 
     @ExceptionHandler(NullPointException.class)
     public ResponseEntity<ErrorResponse> nullPointException(NullPointException exception) {
         logger.error("Invalid request:", exception);
+        alarmSendUseCase.sendException(new AlarmExceptionCommand(exception));
         return ErrorResponse.of(HttpStatus.NOT_FOUND, ErrorCode.NOT_FOUND_ENTITY);
     }
 }
