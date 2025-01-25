571a54acc4e98016a2a1fe7885a84782839142de
unanchoi <unanc2@gmail.com>
2025-01-25 17:35:28 +0900

    hotfix: braindump parameter 수정

--- src/main/kotlin/com/official/memento/todo/service/BrainDumpService.kt
+++ src/main/kotlin/com/official/memento/todo/service/BrainDumpService.kt
@@ -57,10 +57,10 @@ class BrainDumpService(
                 PriorityType.findPriorityType(toDoBrainDump.urgency.toDouble(), toDoBrainDump.importance.toDouble()),
                 ToDoType.NORMAL,
             )
-        toDoRepository.save(toDo)
+        val savedToDo = toDoRepository.save(toDo)
         val tag = tagRepository.findById(DEFAULT_BRAINDUMP_TODO_TAG_ID)
-        toDoTagRepository.save(ToDoTag.of(toDo.id, tag.id))
-        assignOrder(toDoBrainDump.createdDate, toDo)
+        toDoTagRepository.save(ToDoTag.of(savedToDo.id, tag.id))
+        assignOrder(toDoBrainDump.createdDate, savedToDo)
         return toDoBrainDump
     }
 
