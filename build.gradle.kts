37c7f6b670622043d9d8c971ee8e4e065349ca83
smg0725 <smg0725@naver.com>
2025-02-13 23:34:09 +0900

    [FEAT] 구글 캘린더 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,6 +68,11 @@ dependencies {
     // Lombok
     annotationProcessor(libs.lombok)
     compileOnly(libs.lombok)
+
+    // Google Calendar API
+    implementation("com.google.api-client:google-api-client:2.2.0")
+    implementation ("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
+    implementation("com.google.apis:google-api-services-calendar:v3-rev20250115-2.0.0")
 }
 
 kotlin {
