6e2a63f3e2047c0a345cb992817bdc5097f9ba5b
smg0725 <smg0725@naver.com>
2025-02-17 23:23:21 +0900

    [CHORE] 사용하지 않는 메서드 삭제

--- src/main/java/com/official/memento/orderinfo/service/usecase/OrderInfoUpdateUseCase.java
+++ src/main/java/com/official/memento/orderinfo/service/usecase/OrderInfoUpdateUseCase.java
@@ -1,15 +1,9 @@
 package com.official.memento.orderinfo.service.usecase;
 
 import com.official.memento.orderinfo.service.command.ToDoPositionUpdateCommand;
-import com.official.memento.todo.domain.entity.ToDo;
-import java.time.LocalDate;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface OrderInfoUpdateUseCase {
 
     void updatePosition(final ToDoPositionUpdateCommand toDoPositionUpdateCommand);
 
-    @Transactional(propagation = Propagation.REQUIRED)
-    void assignOrder(LocalDate date, ToDo toDo, long memberId);
 }
