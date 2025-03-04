6264342d89e6f9c8eda3f11401463c7ad2541b57
paragon0107 <smg0725@naver.com>
2025-03-04 14:28:00 +0900

    chore: request DTO sync Token 추가

--- src/main/java/com/official/memento/schedule/conntroller/dto/request/AppleSchedulesCreateRequest.java
+++ src/main/java/com/official/memento/schedule/conntroller/dto/request/AppleSchedulesCreateRequest.java
@@ -3,6 +3,11 @@ package com.official.memento.schedule.conntroller.dto.request;
 import java.util.List;
 
 public record AppleSchedulesCreateRequest(
+        String syncToken,
         List<ScheduleCreateRequest> scheduleCreateRequest
 ) {
+    public AppleSchedulesCreateRequest(final String syncToken, final List<ScheduleCreateRequest> scheduleCreateRequest) {
+        this.syncToken = syncToken;
+        this.scheduleCreateRequest = scheduleCreateRequest;
+    }
 }
