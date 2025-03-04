5ca6dda6da1c9bdf4084320842cca774c3642e17
paragon0107 <smg0725@naver.com>
2025-03-04 21:22:35 +0900

    chore: MemberPesonalInfoUseCase 생성

---
+++ src/main/java/com/official/memento/member/service/usecase/MemberPersonalInfoGetUseCase.java
@@ -0,0 +1,9 @@
+package com.official.memento.member.service.usecase;
+
+import com.official.memento.member.domain.MemberPersonalInfo;
+
+import java.util.Optional;
+
+public interface MemberPersonalInfoGetUseCase {
+    Optional<MemberPersonalInfo> findNullableByMemberId(final long memberId);
+}
