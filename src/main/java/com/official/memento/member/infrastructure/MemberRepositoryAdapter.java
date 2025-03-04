d5cf6d6d369a98d10c63a069204712f3ccdce12d
paragon0107 <smg0725@naver.com>
2025-03-04 21:24:39 +0900

    chore: Member Repository 수정

--- src/main/java/com/official/memento/member/infrastructure/MemberRepositoryAdapter.java
+++ src/main/java/com/official/memento/member/infrastructure/MemberRepositoryAdapter.java
@@ -2,24 +2,20 @@ package com.official.memento.member.infrastructure;
 
 import com.official.memento.global.stereotype.Adapter;
 import com.official.memento.member.domain.Member;
-import com.official.memento.member.domain.MemberPersonalInfo;
 import com.official.memento.member.domain.port.MemberRepository;
-import com.official.memento.member.infrastructure.persistence.MemberEntity;
-import com.official.memento.member.infrastructure.persistence.MemberJpaRepository;
-import com.official.memento.member.infrastructure.persistence.MemberPersonalInfoEntity;
-import com.official.memento.member.infrastructure.persistence.MemberPersonalInfoEntityJpaRepository;
+import com.official.memento.member.infrastructure.persistence.entity.MemberEntity;
+import com.official.memento.member.infrastructure.persistence.repository.MemberEntityJpaRepository;
+import com.official.memento.member.infrastructure.persistence.repository.MemberPersonalInfoEntityJpaRepository;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Optional;
 
 @Adapter
+@RequiredArgsConstructor
 public class MemberRepositoryAdapter implements MemberRepository {
-    private final MemberJpaRepository memberJpaRepository;
-    private final MemberPersonalInfoEntityJpaRepository personalInfoRepository;
 
-    public MemberRepositoryAdapter(MemberJpaRepository memberJpaRepository, MemberPersonalInfoEntityJpaRepository personalInfoRepository) {
-        this.memberJpaRepository = memberJpaRepository;
-        this.personalInfoRepository = personalInfoRepository;
-    }
+    private final MemberEntityJpaRepository memberEntityJpaRepository;
+    private final MemberPersonalInfoEntityJpaRepository personalInfoRepository;
 
     @Override
     public Member save(Member member) {
@@ -27,7 +23,7 @@ public class MemberRepositoryAdapter implements MemberRepository {
         entityToSave.setCreatedAt(member.getCreatedAt());
         entityToSave.setUpdatedAt(member.getUpdatedAt());
 
-        MemberEntity savedEntity = memberJpaRepository.save(entityToSave);
+        MemberEntity savedEntity = memberEntityJpaRepository.save(entityToSave);
 
         return new Member(
                 savedEntity.getId(),
@@ -38,28 +34,11 @@ public class MemberRepositoryAdapter implements MemberRepository {
 
     @Override
     public Optional<Member> findById(Long id) {
-        return memberJpaRepository.findById(id)
+        return memberEntityJpaRepository.findById(id)
                 .map(entity -> new Member(
                         entity.getId(),
                         entity.getCreatedAt(),
                         entity.getUpdatedAt()
-                ));
-    }
-
-    @Override
-    public Optional<MemberPersonalInfo> findPersonalInfoByMemberId(Long memberId) {
-        return personalInfoRepository.findByMemberId(memberId)
-                .map(entity -> new MemberPersonalInfo(
-                        entity.getId(),
-                        entity.getMemberId(),
-                        entity.getWakeUpTime(),
-                        entity.getWindDownTime(),
-                        entity.getJob(),
-                        entity.getJobOtherDetail(),
-                        entity.getIsStressedUnorganizedSchedule(),
-                        entity.getIsForgetImportantThings(),
-                        entity.getIsPreferReminder(),
-                        entity.getIsImportantBreaks()
                 ));
     }
 }
