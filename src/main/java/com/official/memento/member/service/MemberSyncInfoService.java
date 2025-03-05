54ba02c2a0ba1d82c6c22b98df967e765e202f4a
smg0725 <smg0725@naver.com>
2025-03-06 00:28:28 +0900

    feat: MemberSyncInfo 서비스 메서드 추가

--- src/main/java/com/official/memento/member/service/MemberSyncInfoService.java
+++ src/main/java/com/official/memento/member/service/MemberSyncInfoService.java
@@ -4,7 +4,9 @@ import com.official.memento.global.exception.DataBaseIntegrityException;
 import com.official.memento.member.domain.MemberSyncInfo;
 import com.official.memento.member.domain.port.MemberSyncInfoRepository;
 import com.official.memento.member.service.command.MemberSyncInfoCreateCommand;
+import com.official.memento.member.service.command.MemberSyncInfoGetUseCase;
 import com.official.memento.member.service.usecase.MemberSyncInfoCreateUseCase;
+import com.official.memento.member.service.usecase.MemberSyncInfoUpdateUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +15,11 @@ import static com.official.memento.global.exception.ErrorCode.DB_INTEGRITY_CONFL
 
 @Service
 @RequiredArgsConstructor
-public class MemberSyncInfoService implements MemberSyncInfoCreateUseCase {
+public class MemberSyncInfoService implements
+        MemberSyncInfoCreateUseCase,
+        MemberSyncInfoGetUseCase,
+        MemberSyncInfoUpdateUseCase
+{
 
     private final MemberSyncInfoRepository memberSyncInfoRepository;
 
@@ -26,8 +32,21 @@ public class MemberSyncInfoService implements MemberSyncInfoCreateUseCase {
         return memberSyncInfo;
     }
 
+    @Override
+    @Transactional(readOnly = true)
+    public MemberSyncInfo findByMemberId(final long memberId){
+        return memberSyncInfoRepository.findByMemberId(memberId);
+    }
+
+    //Todo update로직 고민
+    @Override
+    public void updateAppleToken(long memberId, String token) {
+        MemberSyncInfo memberSyncInfo = memberSyncInfoRepository.findByMemberId(memberId);
+        memberSyncInfoRepository.save(memberSyncInfo.updateAppleToken(token));
+    }
+
     private void checkPresentByMemberId(final MemberSyncInfoCreateCommand command) {
-        if(memberSyncInfoRepository.findByMemberId(command.memberId()).isPresent()){
+        if(memberSyncInfoRepository.findNullableByMemberId(command.memberId()).isPresent()){
             throw new DataBaseIntegrityException(DB_INTEGRITY_CONFLICT);
         }
     }
