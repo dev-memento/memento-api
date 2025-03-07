79b237f9320fd677321c2f4d7c046492d055a5c6
smg0725 <smg0725@naver.com>
2025-03-07 20:25:16 +0900

    chore: AppleSchedulesRequest 수정

--- src/main/java/com/official/memento/schedule/conntroller/dto/request/AppleSchedulesRequest.java
+++ src/main/java/com/official/memento/schedule/conntroller/dto/request/AppleSchedulesRequest.java
@@ -7,17 +7,14 @@ import java.util.List;
 
 @Schema(name = "애플 일정 생성 요청")
 public record AppleSchedulesRequest(
-        long memberId,
         String syncToken,
         List<AppleScheduleRequest> scheduleCreateRequest
 ) {
     public AppleSchedulesRequest(
-            final long memberId,
             final String syncToken,
             final List<AppleScheduleRequest> scheduleCreateRequest
     ) {
         Validator.isNull(syncToken);
-        this.memberId = memberId;
         this.syncToken = syncToken;
         this.scheduleCreateRequest = scheduleCreateRequest;
     }
