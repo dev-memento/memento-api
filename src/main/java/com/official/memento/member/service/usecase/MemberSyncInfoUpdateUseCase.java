7749748d826d3ddcdd8398b79fdc8564c24daa94
smg0725 <smg0725@naver.com>
2025-03-07 21:36:45 +0900

    chore: MemberSyncInfo apple 싱크 업데이트 메서드명 변경

--- src/main/java/com/official/memento/member/service/usecase/MemberSyncInfoUpdateUseCase.java
+++ src/main/java/com/official/memento/member/service/usecase/MemberSyncInfoUpdateUseCase.java
@@ -1,5 +1,5 @@
 package com.official.memento.member.service.usecase;
 
 public interface MemberSyncInfoUpdateUseCase {
-    void updateAppleToken(final long memberId,final String token);
+    void toggleAppleSync(final long memberId);
 }
