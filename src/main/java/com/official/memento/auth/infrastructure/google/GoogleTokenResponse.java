7028c74a10469704501e28150d2648708915d775
smg0725 <smg0725@naver.com>
2025-03-21 19:43:20 +0900

    chore: 컨벤션 수정

--- src/main/java/com/official/memento/auth/infrastructure/google/GoogleTokenResponse.java
+++ src/main/java/com/official/memento/auth/infrastructure/google/GoogleTokenResponse.java
@@ -1,11 +1,8 @@
 package com.official.memento.auth.infrastructure.google;
 
 public record GoogleTokenResponse(
-
         String accessToken,
-
         int expiresIn,
-
         String scope,
         String tokenType
 ) {
