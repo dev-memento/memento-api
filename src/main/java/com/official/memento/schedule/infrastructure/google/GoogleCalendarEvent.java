b42f99f18ec95e550b8fd3ca30c2ddf348228cfe
smg0725 <smg0725@naver.com>
2025-03-21 17:35:49 +0900

    chore: 구글 캘린더 id로직 수정

--- src/main/java/com/official/memento/schedule/infrastructure/google/GoogleCalendarEvent.java
+++ src/main/java/com/official/memento/schedule/infrastructure/google/GoogleCalendarEvent.java
@@ -2,10 +2,8 @@ package com.official.memento.schedule.infrastructure.google;
 
 import com.official.memento.global.entity.enums.RepeatOption;
 import com.official.memento.schedule.domain.entity.Schedule;
-import com.official.memento.schedule.domain.entity.ScheduleVo;
 import com.official.memento.schedule.domain.enums.ScheduleType;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Locale;
 
@@ -20,24 +18,20 @@ public record GoogleCalendarEvent(
 ) {
     public Schedule toSchedule(final long memberId, final long tagId) {
         RepeatOption repeatOption = RepeatOption.NONE;
+        LocalDate repeatEndDate = null;
         if (this.recurrence != null) {
             for (String rule : this.recurrence) {
                 if (rule.contains("FREQ=")) {
                     String freq = rule.substring(rule.indexOf("FREQ=") + 5, rule.indexOf(";"));
                     repeatOption = RepeatOption.valueOf(freq.toUpperCase(Locale.ROOT));
                 }
-            }
-        }
-
-        LocalDate repeatEndDate = null;
-        if (this.recurrence != null) {
-            for (String rule : this.recurrence) {
                 if (rule.contains("UNTIL=")) {
                     String until = rule.substring(rule.indexOf("UNTIL=") + 6, rule.indexOf("T"));
                     repeatEndDate = LocalDate.parse(until);
                 }
             }
         }
+
         return Schedule.of(
                 memberId,
                 this.summary() != null ? this.summary() : "", // description
@@ -47,9 +41,7 @@ public record GoogleCalendarEvent(
                 repeatOption,
                 repeatEndDate,
                 ScheduleType.GOOGLE,
-                this.recurringEventId() != null ? this.recurringEventId() +
-                        this.start().toLocalDateTime() +
-                        this.end().toLocalDateTime() : this.id(),
+                this.id(),
                 tagId
         );
     }
