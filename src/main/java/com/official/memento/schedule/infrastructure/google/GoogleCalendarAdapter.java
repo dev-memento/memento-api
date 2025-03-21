bbe4ec7b4fdbef50492ea506340983e831acb878
smg0725 <smg0725@naver.com>
2025-03-21 17:35:15 +0900

    chore: 구글 캘린더 get 로직에 syncToken 반영

--- src/main/java/com/official/memento/schedule/infrastructure/google/GoogleCalendarAdapter.java
+++ src/main/java/com/official/memento/schedule/infrastructure/google/GoogleCalendarAdapter.java
@@ -1,16 +1,17 @@
 package com.official.memento.schedule.infrastructure.google;
 
 import com.official.memento.global.exception.ErrorCode;
-import com.official.memento.global.exception.ExpiredTokenException;
-import com.official.memento.global.exception.InvalidRequestBodyException;
 import com.official.memento.global.exception.MementoException;
-import java.util.Optional;
 import lombok.AllArgsConstructor;
-import org.springframework.http.*;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 @AllArgsConstructor
@@ -18,27 +19,45 @@ public class GoogleCalendarAdapter {
 
     private final RestTemplate restTemplate;
 
-    public GoogleCalendarResponse getCalendarEvents(final String accessToken,final String syncToken) {
-        final String GOOGLE_CALENDAR_EVENTS_URL = "https://www.googleapis.com/calendar/v3/calendars/primary/events";
-
-        HttpHeaders headers = createAuthHeaders(accessToken);
-        HttpEntity<String> request = new HttpEntity<>(headers);
-
-        ResponseEntity<GoogleCalendarResponse> response = restTemplate.exchange(
-                GOOGLE_CALENDAR_EVENTS_URL,
-                HttpMethod.GET,
-                request,
-                GoogleCalendarResponse.class
+    public GoogleCalendarResponse getCalendarEvents(final String accessToken, final String syncToken) {
+        ResponseEntity<GoogleCalendarResponse> response = getGoogleCalendarResponseResponseEntity(
+                accessToken,
+                syncToken
         );
 
-        if (response.getStatusCode() == HttpStatus.UNAUTHORIZED) {
-            throw new InvalidRequestBodyException(ErrorCode.EXPIRED_TOKEN);
+        if (response.getStatusCode() == HttpStatus.GONE) {
+            response = getGoogleCalendarResponseResponseEntity(accessToken, null);
         }
         if (!response.getStatusCode().is2xxSuccessful()) {
             throw new MementoException(ErrorCode.EXTERNAL_SERVER_ERROR);
         }
 
         return response.getBody();
+    }
+
+    @NotNull
+    private ResponseEntity<GoogleCalendarResponse> getGoogleCalendarResponseResponseEntity(
+            String accessToken,
+            String syncToken) {
+        final String url = "https://www.googleapis.com/calendar/v3/calendars/primary/events";
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url)
+                .queryParam("showDeleted", "true")
+                .queryParam("singleEvents", "true");
+        if (syncToken != null) {
+            builder.queryParam("syncToken", syncToken).queryParam("showDeleted", "false");
+        }
+
+        HttpHeaders headers = createAuthHeaders(accessToken);
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        ResponseEntity<GoogleCalendarResponse> response = restTemplate.exchange(
+                builder.toUriString(),
+                HttpMethod.GET,
+                request,
+                GoogleCalendarResponse.class
+        );
+        return response;
     }
 
     private HttpHeaders createAuthHeaders(final String accessToken) {
