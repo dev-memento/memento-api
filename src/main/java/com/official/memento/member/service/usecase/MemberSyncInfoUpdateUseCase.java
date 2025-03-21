1fc15d45b6bd9266b4882f012d29a5e9c87965f2
smg0725 <smg0725@naver.com>
2025-03-21 17:36:52 +0900

    feat: updateGoogleSyncToken 메서드 추가

--- src/main/java/com/official/memento/member/service/usecase/MemberSyncInfoUpdateUseCase.java
+++ src/main/java/com/official/memento/member/service/usecase/MemberSyncInfoUpdateUseCase.java
@@ -2,4 +2,5 @@ package com.official.memento.member.service.usecase;
 
 public interface MemberSyncInfoUpdateUseCase {
     void activateAppleSync(final long memberId);
+    void updateGoogleSyncToken(final long memberId, final String googleSyncToken);
 }
