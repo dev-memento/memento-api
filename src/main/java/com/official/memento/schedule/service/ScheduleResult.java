c93edf7ca595c6010d063ddcff5673c2997e7c22
paragon0107 <smg0725@naver.com>
2025-03-24 14:53:07 +0900

    feat: 스케줄 result 생성

---
+++ src/main/java/com/official/memento/schedule/service/ScheduleResult.java
@@ -0,0 +1,36 @@
+package com.official.memento.schedule.service;
+
+import com.official.memento.global.entity.enums.RepeatOption;
+import com.official.memento.schedule.domain.entity.Schedule;
+import com.official.memento.schedule.domain.enums.ScheduleType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record ScheduleResult(
+        long scheduleId,
+        String description,
+        LocalDateTime startDate,
+        LocalDateTime endDate,
+        boolean isAllDay,
+        long tagId,
+        ScheduleType scheduleType,
+        String scheduleGroupId,
+        RepeatOption repeatOption,
+        LocalDate repeatEndDate
+) {
+    public static ScheduleResult of(final Schedule schedule) {
+        return new ScheduleResult(
+                schedule.getId(),
+                schedule.getDescription(),
+                schedule.getStartDate(),
+                schedule.getEndDate(),
+                schedule.isAllDay(),
+                schedule.getTagId(),
+                schedule.getType(),
+                schedule.getScheduleGroupId(),
+                schedule.getRepeatOption(),
+                schedule.getRepeatExpiredDate()
+        );
+    }
+}
