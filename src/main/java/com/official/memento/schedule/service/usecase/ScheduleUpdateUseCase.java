d5f696e4b10b162d8f588b97ab37c1c5436a8c17
paragon0107 <smg0725@naver.com>
2025-03-24 14:52:48 +0900

    chore: 업데이트 useCase 반환형 수정

--- src/main/java/com/official/memento/schedule/service/usecase/ScheduleUpdateUseCase.java
+++ src/main/java/com/official/memento/schedule/service/usecase/ScheduleUpdateUseCase.java
@@ -1,8 +1,9 @@
 package com.official.memento.schedule.service.usecase;
 
+import com.official.memento.schedule.service.ScheduleResult;
 import com.official.memento.schedule.service.command.ScheduleUpdateCommand;
 
 public interface ScheduleUpdateUseCase {
-    void update(final ScheduleUpdateCommand command);
+    ScheduleResult update(final ScheduleUpdateCommand command);
     void updateGoogle(final ScheduleUpdateCommand command);
 }
