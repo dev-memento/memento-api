a008577aad460ee0fbd9e0b55d7a03561a611fc0
smg0725 <smg0725@naver.com>
2025-04-02 13:00:41 +0900

    chore: 스케줄 알람 시간 추가

--- src/main/java/com/official/memento/schedule/scheduler/scheduleAlarmScheduler.java
+++ src/main/java/com/official/memento/schedule/scheduler/scheduleAlarmScheduler.java
@@ -1,7 +1,11 @@
 package com.official.memento.schedule.scheduler;
 
+import com.official.memento.schedule.domain.entity.Schedule;
 import com.official.memento.schedule.service.CloudTaskAdapter;
 import com.official.memento.schedule.service.usecase.ScheduleGetUseCase;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -16,7 +20,8 @@ public class scheduleAlarmScheduler {
 
     @Scheduled(cron = "0 30 0 * * *", zone = "UTC") // 매일 UTC 기준 0시 30분
     public void setScheduleAlarm() {
-
-        scheduleGetUseCase.getSchedulesBetween()
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("UTC"));
+        LocalDateTime tomorrow = now.plusDays(1);
+        List<Schedule> schedules = scheduleGetUseCase.getSchedulesBetween(now, tomorrow);
     }
 }
