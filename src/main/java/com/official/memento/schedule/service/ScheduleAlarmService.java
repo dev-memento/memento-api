d34aa775516e3a55ccc415713c1ad4ebca9f9f9f
paragon0107 <smg0725@naver.com>
2025-04-05 10:48:04 +0900

    feat: ScheduleAlarmService 구현

---
+++ src/main/java/com/official/memento/schedule/service/ScheduleAlarmService.java
@@ -0,0 +1,22 @@
+package com.official.memento.schedule.service;
+
+import com.official.memento.schedule.domain.ScheduleRepository;
+import com.official.memento.schedule.domain.entity.ScheduleAlarm;
+import com.official.memento.schedule.service.usecase.ScheduleAlarmGetUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleAlarmService implements ScheduleAlarmGetUseCase {
+
+    private final ScheduleRepository scheduleRepository;
+
+    @Override
+    public List<ScheduleAlarm> getSchedulesBetween(final LocalDateTime startDateTime, final LocalDateTime endDateTime) {
+        return scheduleRepository.findSchedulesWithMemberInfoBetween(startDateTime, endDateTime);
+    }
+}
