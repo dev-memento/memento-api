6e8fa80601646ef95071bf591612da652ced1486
paragon0107 <smg0725@naver.com>
2025-04-05 10:45:13 +0900

    feat: ScheduleAlarmCustomRepository 부분 구현

---
+++ src/main/java/com/official/memento/schedule/infrastructure/persistence/ScheduleAlarmCustomRepository.java
@@ -0,0 +1,49 @@
+package com.official.memento.schedule.infrastructure.persistence;
+
+import com.official.memento.member.domain.MemberPersonalInfo;
+import com.official.memento.member.infrastructure.persistence.entity.MemberPersonalInfoEntity;
+import com.official.memento.member.infrastructure.persistence.entity.QMemberPersonalInfoEntity;
+import com.official.memento.schedule.infrastructure.persistence.projection.ScheduleAlarmProjection;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Repository
+@RequiredArgsConstructor
+public class ScheduleAlarmCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+    QScheduleEntity schedule = QScheduleEntity.scheduleEntity;
+    QMemberPersonalInfoEntity memberPersonalInfo = QMemberPersonalInfoEntity.memberPersonalInfoEntity;
+
+    public List<ScheduleAlarmProjection> findSchedulesWithMemberInfoBetween(
+            final LocalDateTime startDate,
+            final LocalDateTime endDate
+    ) {
+        List<Tuple> results = queryFactory
+                .select(schedule.id, schedule.memberId, schedule.description, schedule.startDate, schedule.endDate, memberPersonalInfo.timeZoneOffset)
+                .from(schedule)
+                .join(memberPersonalInfo).on(schedule.memberId.eq(memberPersonalInfo.memberId))
+                .where(schedule.startDate.between(startDate, endDate))
+                .fetch();
+
+        return results.stream()
+                .map(tuple -> new ScheduleAlarmProjection(
+                        tuple.get(schedule.id),
+                        tuple.get(schedule.memberId),
+                        tuple.get(schedule.description),
+                        tuple.get(schedule.startDate),
+                        tuple.get(schedule.endDate),
+                        tuple.get(memberPersonalInfo.timeZoneOffset)
+                ))
+                .collect(Collectors.toList());
+    }
+
+
+}
