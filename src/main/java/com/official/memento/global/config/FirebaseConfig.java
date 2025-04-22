c532be09988aa23788335fa212d7b039c066164a
paragon0107 <smg0725@naver.com>
2025-04-22 20:01:52 +0900

    chore: firebase secret키 임포트 방식 수정

--- src/main/java/com/official/memento/global/config/FirebaseConfig.java
+++ src/main/java/com/official/memento/global/config/FirebaseConfig.java
@@ -5,24 +5,32 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.config-path}")
+    private String firebaseConfigPath;
+
     @PostConstruct
     public void initFirebase() throws IOException {
+        System.out.println(firebaseConfigPath);
         if (FirebaseApp.getApps().isEmpty()) {
-            InputStream serviceAccount = getClass().getResourceAsStream("/fire/firebase-admin-sdk.json");
-
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            FirebaseApp.initializeApp(options);
+            try (InputStream serviceAccount = new FileInputStream(firebaseConfigPath)) {
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+                FirebaseApp.initializeApp(options);
+            }
         }
     }
 }
+
 
